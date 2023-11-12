7eb85cea1696171e8f6af38662e20c9fe7d37daf
David <drvillase@gmail.com>
2023-11-12 04:35:12 -0500

    Docker changes

--- Scripts/Build_Debug_x86_64.ps1
+++ Scripts/Build_Debug_x86_64.ps1
@@ -2,16 +2,11 @@ $ImageName = "vimdows-buildenv"
 $ContainerName = "Vimdows-Build"
 
 $RootDir = [IO.Path]::GetFullPath("..\")
-$TargetDir = "/dev/buildenv"
+$TargetDir = "/dev/host"
 $DockerfilePath = $RootDir + "Buildenv"
 
-$Images = docker images $ImageName
-
-if ($Images.Count -le 1)
-{
-    docker build $DockerfilePath -t $ImageName
-}
-
+docker image prune --force
+docker build --no-cache $DockerfilePath -t $ImageName
 docker run -it --rm --name=$ContainerName --mount type=bind,source=$RootDir,target=$TargetDir $ImageName
 
 pause
