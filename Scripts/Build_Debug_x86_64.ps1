cae927826c6ec8c16bb37628b619ab705e529608
David <drvillase@gmail.com>
2023-11-13 04:43:06 -0500

    Updated Dockerfile

--- Scripts/Build_Debug_x86_64.ps1
+++ Scripts/Build_Debug_x86_64.ps1
@@ -2,11 +2,11 @@ $ImageName = "vimdows-buildenv"
 $ContainerName = "Vimdows-Build"
 
 $RootDir = [IO.Path]::GetFullPath("..\")
-$TargetDir = "/dev/host"
+$TargetDir = "/root/host"
 $DockerfilePath = $RootDir + "Buildenv"
 
 docker image prune --force
-docker build --no-cache $DockerfilePath -t $ImageName
+docker build $DockerfilePath -t $ImageName
 docker run -it --rm --name=$ContainerName --mount type=bind,source=$RootDir,target=$TargetDir $ImageName
 
 pause
